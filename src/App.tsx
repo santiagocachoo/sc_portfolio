05f880b75f8aee9f172f2996fdbde6756efcd699
santiago <a01255853@tec.mx>
2026-05-29 18:26:33 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <h1 className="text-4xl font-bold p-8">Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
