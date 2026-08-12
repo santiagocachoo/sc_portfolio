2bfaaec0cc95cb5eeb807e32fd37cdb869e1e30e
santiago <a01255853@tec.mx>
2026-08-12 12:52:15 -0600

    chore: vercel analytics setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './pages/Home'
 import ProjectDetail from './pages/ProjectDetail'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects/:slug" element={<ProjectDetail />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:slug" element={<ProjectDetail />} />
+      </Routes>
+      <Analytics />
+    </>
   )
 }
